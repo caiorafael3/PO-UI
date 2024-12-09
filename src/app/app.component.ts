890529c1e15752a7ee22fbbdb75408f565278af8
Caio Rafael <caio.rafael@guaraves.local>
2024-12-09 12:57:05 -0300

    Enviando Versão 1.0 de teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProAppConfigService } from '@totvs/protheus-lib-core';
+import { EditorComponent } from './Components/Editor/Editor';
+
+import {
+  PoMenuItem,
+  PoMenuModule,
+  PoPageModule,
+  PoToolbarModule,
+  PoModule,
+} from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    PoToolbarModule,
+    PoMenuModule,
+    PoPageModule,
+    HttpClientModule,
+    PoModule,
+    EditorComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'my-protheus-app';
+  constructor(private propAppConfigService: ProAppConfigService){
+    if (!this.propAppConfigService.insideProtheus){
+      this.propAppConfigService.loadAppConfig()
+    }
+  }
+
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Home', action: this.onClick.bind(this) },
+    { label: 'Tabelas',},
+    { label: 'Exit', action: this.closeApp.bind(this) },
+  ];
+
+  private onClick() {
+    alert('Clicked in menu item');
+  }
+
+  private closeApp() {
+    if (this.propAppConfigService.insideProtheus()){
+      this.propAppConfigService.callAppClose();
+    } else {
+      alert('O app não está sendo executado dentro do Protheus.')
+    }
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PoHttpRequestModule } from '@po-ui/ng-components';
+import { PoTemplatesModule } from '@po-ui/ng-templates';
+import { ProtheusLibCoreModule } from '@totvs/protheus-lib-core';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom([BrowserAnimationsModule, PoHttpRequestModule, PoTemplatesModule, ProtheusLibCoreModule]),
+  ],
+  
 };
